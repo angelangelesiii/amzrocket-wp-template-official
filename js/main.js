b9159eaac3a35955d086154c1e206894c818fb29
Angel Angeles <angelangelesiii@gmail.com>
2017-06-15 08:44:20 +0800

    Added animation on scroll for navbar

--- js/main.js
+++ js/main.js
@@ -9,6 +9,21 @@ jQuery(document).ready(function($){ // Document Ready
 	//  MAIN
 	// ===================================================
 
+	var mainController = new ScrollMagic.Controller();
+
+	var NavBarScroll = new ScrollMagic.Scene({
+		triggerElement: '.hero',
+		offset: 50,
+		triggerHook: 0
+	})
+	.on('enter', function() {
+		$('#mainheader').removeClass('top-position');
+	})
+	.on('leave', function() {
+		$('#mainheader').addClass('top-position');
+	})
+	// .addIndicators()
+	.addTo(mainController);
 	
 
 });
