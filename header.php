acf33164e6b512f9dda0d71fb847539bf51f8924
Angel Angeles <angelangelesiii@gmail.com>
2017-06-23 21:38:04 +0800

    Added theme color for chrome

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="theme-color" content="#E80027">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
